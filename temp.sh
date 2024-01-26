d03d4c5c36ffb1432a3b19f7419955e3f24fb9bc
Ardelean Calin <9417983+Ardelean-Calin@users.noreply.github.com>
2024-01-26 10:50:18 +0200

    update temp.sh

--- temp.sh
+++ temp.sh
@@ -3,42 +3,19 @@
 set -e
 
 KERNEL_VERSION=6.1.74
-BUSYBOX_VERSION=1.36.1
+UROOT_VERSION=0.12.0
 
 
 # Create initrd
-mkdir -p initrd
-cd initrd || exit
+cd src || exit
+  wget -O uroot.zip "https://github.com/u-root/u-root/archive/refs/tags/v$UROOT_VERSION.zip"
+  unzip uroot.zip
+  rm uroot.zip
 
-  echo '#!/bin/sh' > init
-  {
-    echo 'mount -t sysfs sysfs /sys'
-    echo 'mount -t proc proc /proc'
-    echo 'mount -t devtmpfs udev /dev'
-    echo 'sysctl -w kernel.printk="2 4 1 7"'
-    echo 'clear'
-    echo "cat <<!
+  cd "u-root-$UROOT_VERSION" || exit
 
+    go build
+    ./u-root -o ../../initramfs.linux_amd64.cpio
 
-Boot took $(cut -d' ' -f1 /proc/uptime) seconds
-
-   ___        _  _           __  _                     
-  / __\ __ _ | |(_) _ __    / / (_) _ __   _   _ __  __
- / /   / _' || || || '_ \  / /  | || '_ \ | | | |\ \/ /
-/ /___| (_| || || || | | |/ /___| || | | || |_| | >  < 
-\____/ \__,_||_||_||_| |_|\____/|_||_| |_| \__,_|/_/\_\ 
-
-
-Welcome to Calin Linux
-
-
-!"
-    echo "/bin/sh"
-    echo "poweroff -f"
-  } >> init
-
-  chmod -R 777 .
-
-  find . | cpio -o -H newc > ../initrd.img
-
+  cd ..
 cd ..
