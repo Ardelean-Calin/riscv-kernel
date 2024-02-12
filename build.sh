99510cb742edb1fcd3129094b52864235820dcca
Calin <9417983+Ardelean-Calin@users.noreply.github.com>
2024-02-12 14:38:12 +0200

    Added Go install phase and qemu support.

--- build.sh
+++ build.sh
@@ -4,15 +4,26 @@ set -e
 
 KERNEL_VERSION=6.1.74
 UROOT_VERSION=0.12.0
+GO_VERSION=1.22.0
 
+# Prerequisites
+mkdir -p tools
+export PATH=$PATH:$(pwd)/tools/go/bin
+
+# Download and install go
+cd tools || exit
+  wget "https://go.dev/dl/go$GO_VERSION.linux-amd64.tar.gz" 
+  rm -rf go && tar -C ./ -xzf go$GO_VERSION.linux-amd64.tar.gz
+cd ..
+
+# Download and build the Kernel
 mkdir -p src
 cd src || exit
 
   # Download the linux kernel
   KERNEL_MAJOR=$(echo $KERNEL_VERSION | sed 's/\(\d*\)\..*/\1/')
-  wget -O linux-kernel.tar.xz "https://cdn.kernel.org/pub/linux/kernel/v$KERNEL_MAJOR.x/linux-$KERNEL_VERSION.tar.xz"
+  wget -O linux-kernel.tar.xz "https://cdn.kernel.org/pub/linux/kernel/v$KERNEL_MAJOR.x/linux-$KERNEL_VERSION.tar.xz" 
   tar xf linux-kernel.tar.xz
-  rm linux-kernel.tar.xz
 
   # Build the Linux Kernel
   cd "linux-$KERNEL_VERSION" || exit
@@ -22,9 +33,8 @@ cd src || exit
   cd ..
 
   # Download U-Root userland
-  wget -O uroot.zip "https://github.com/u-root/u-root/archive/refs/tags/v$UROOT_VERSION.zip"
-  unzip uroot.zip
-  rm uroot.zip
+  wget -O uroot.zip "https://github.com/u-root/u-root/archive/refs/tags/v$UROOT_VERSION.zip" 
+  unzip -n uroot.zip
 
   # Create initramfs (binary root file system)
   cd "u-root-$UROOT_VERSION" || exit
@@ -34,6 +44,7 @@ cd src || exit
   
 cd ..
 
+# Done. Print simulation command.
 GREEN='\033[0;32m'
 NC='\033[0m' 
 echo -e "${GREEN}Done! To test your kernel you can run:${NC}\n  qemu-system-x86_64 -kernel bzImage -nographic -serial mon:stdio -append 'console=ttyS0' -m 512 -initrd initramfs.linux_amd64.cpio"
--- temp.sh
+++ temp.sh
@@ -4,18 +4,19 @@ set -e
 
 KERNEL_VERSION=6.1.74
 UROOT_VERSION=0.12.0
+GO_VERSION=1.22.0
 
 
+mkdir -p tools
+cd tools || exit
+
+cd ..
 # Create initrd
 cd src || exit
-  wget -O uroot.zip "https://github.com/u-root/u-root/archive/refs/tags/v$UROOT_VERSION.zip"
-  unzip uroot.zip
-  rm uroot.zip
 
-  cd "u-root-$UROOT_VERSION" || exit
+  wget "https://go.dev/dl/go$GO_VERSION.linux-amd64.tar.gz" 
+  mkdir -p ../tools
+  rm -rf ../tools/go && tar -C ../tools -xzf go$GO_VERSION.linux-amd64.tar.gz
 
-    go build
-    ./u-root -o ../../initramfs.linux_amd64.cpio
-
-  cd ..
+  ../tools/go/bin/go version
 cd ..
